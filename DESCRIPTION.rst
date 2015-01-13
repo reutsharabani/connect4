51a8e98d88015e97f5dd4bc55590d82d497c8ed0
reut <reut.sharabani@gmail.com>
2015-01-13 03:12:27 +0200

    pypi working version 0.0.1

--- DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This is a fun project I started while there were too many people in my nephew's 6th birthday. It is now playable so I
-decided to share it for the greater good.
--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
 __author__ = 'reut'
 
 from setuptools import setup, find_packages
-from codecs import open
 from os import path
 here = path.abspath(path.dirname(__file__))
 # Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-    setup(
-        name='ninarow',
-        version='0.0.1',
-        description='N-in-a-row Game',
-        long_description='Simple implementation for the N-in-a-row (originally 4-in-a-row)'
-                         ' classic game using tkinter GUI',
-        url='https://github.com/reutsharabani/N-in-a-row',
-        # Author details
-        author='Reut Sharabani',
-        author_email='reut.sharabani@gmail.com',
-        # Choose your license
-        license='MIT',
-        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        classifiers=[
-            'Development Status :: 2 - Pre-Alpha',
-            'Intended Audience :: End Users/Desktop',
-            'Topic :: Games/Entertainment :: Board Games',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7'
-        ],
-        # What does your project relate to?
-        keywords='Simple and fun board game to play with younger members of family.',
-        # You can just specify the packages manually here if your project is
-        #  simple. Or you can use find_packages().
-        packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-        install_requires=['']
+setup(
+    name='ninarow',
+    version='0.0.1',
+    description='N-in-a-row Game',
+    long_description='Simple implementation for the N-in-a-row (originally 4-in-a-row)'
+                     ' classic game using tkinter GUI',
+    url='https://github.com/reutsharabani/N-in-a-row',
+    # Author details
+    author='Reut Sharabani',
+    author_email='reut.sharabani@gmail.com',
+    # Choose your license
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment :: Board Games',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    # What does your project relate to?
+    keywords='Simple and fun board game to play with younger members of family.',
+    # You can just specify the packages manually here if your project is
+    #  simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['']
 )
